c9485cfc03f13cc80071b51aa8114f9041046278
Ahmad0301 <sehroz3040@gmail.com>
2024-11-10 18:22:44 +0500

    assignment 01

---
+++ data.js
@@ -0,0 +1,8 @@
+ const books = 
+[
+    { id: 1, title: "Book 1", author: "Author A", isAvailable: true, issuedTo: null },
+    { id: 2, title: "Book 2", author: "Author B", isAvailable: false, issuedTo: "Member 1" },
+    { id: 3, title: "Book 3", author: "Author A", isAvailable: true, issuedTo: null },
+    { id: 4, title: "Book 4", author: "Author C", isAvailable: false, issuedTo: "Member 2" },
+];
+  module.exports = {books};
---
+++ index.js
@@ -0,0 +1,31 @@
+const express =require ('express')
+const app = express()
+const data = require('./data.js')
+
+
+
+
+app.get('/api/books',(req,resp)=>{
+    resp.json(data.books)
+})
+
+app.get('/api/books/avalible',(req,resp)=>{
+    const ab =data.books.filter(books => books.isAvailable)
+    resp.json(ab)
+})
+
+app.get('/api/books/issued',(req,resp)=>{
+    const ib =data.books.filter(books => books.issuedTo)
+    resp.json(ib)
+})
+
+app.get('/api/books?author=AuthorName',(req,resp)=>{
+    const an =data.books.filter(books =>books.author )
+    resp.json(an)
+})
+
+const port =3000
+app.listen(port,()=>{
+    console.log("server is open now")
+})
+
